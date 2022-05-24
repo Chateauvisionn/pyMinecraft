91af868224fd6a240ed128ac41fcfd17dac498bd
Chateauvision <89737965+Chateauvisionn@users.noreply.github.com>
2022-05-24 21:28:48 +0200

    added menu

--- src/menu.py
+++ src/menu.py
@@ -4,4 +4,13 @@ app = Ursina()
 window.borderless = False
 window.exit_button.visible = False
 
+print("Menu isn't ready yet, start the game with game.py (args for world gen: \"flat\" or \"random\")")
+
+buttonsd = {
+    "Random generated world": None,
+    "Flat world": None,
+}
+
+buttons = ButtonList(buttonsd)
+
 app.run()
